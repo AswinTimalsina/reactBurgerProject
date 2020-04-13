42684ee24da4acd43d788ac9eb3c0ae7c160262d
Aswin Timalsina <aswin.timalsina1@gmail.com>
2020-04-13 13:32:51 -0500

    124. Create a layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './App.module.css';
+import Layout from './components/Layout/Layout';
+
 function App() {
   return (
-    <div className={styles.hello}>
-      Aswin Timalsina
+    <div>
+      <Layout><p>Hi there!!</p></Layout>
     </div>
   );
 }
 
 export default App;
+ 
