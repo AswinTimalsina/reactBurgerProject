da9dba234fd3102cff07812f5d1b843b125ea566
Aswin Timalsina <aswin.timalsina1@gmail.com>
2020-05-30 09:46:25 -0500

    221. Building Checkout Container

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from './App.module.css';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 function App() {
   return (
     <div>
       <Layout>
         <BurgerBuilder/>
+        <Checkout />
         </Layout>
     </div>
   );
