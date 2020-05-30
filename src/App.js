3958e0a4f85f99b7d29b5de6539d4678cb465ae6
Aswin Timalsina <aswin.timalsina1@gmail.com>
2020-05-30 10:18:24 -0500

    222. Setup Routing and Routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import styles from './App.module.css';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Layout>
-        <BurgerBuilder/>
-        <Checkout />
+        <Switch>
+        <Route path='/' exact component={BurgerBuilder}/>
+        <Route path='/checkout' component={Checkout} />
+        </Switch>
+        
         </Layout>
     </div>
   );
