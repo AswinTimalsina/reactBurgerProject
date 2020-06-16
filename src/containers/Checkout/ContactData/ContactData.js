39405a2eecde4593f5da620473f76b901f3cb6f8
Aswin Timalsina <aswin.timalsina1@gmail.com>
2020-06-16 10:15:11 -0500

    242. 243. Common Validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -15,7 +15,11 @@ state={
                 type: 'text',
                 placeholder: 'Your Name'
             },
-            value: ''
+            value: '',
+            validation:{
+                required: true
+            },
+            valid: false
         },
         email: {
             elementType: 'input',
@@ -23,7 +27,11 @@ state={
                 type: 'email',
                 placeholder: 'Your Email'
             },
-            value: ''
+            value: '',
+            validation:{
+                required: true
+            },
+            valid: false
         },
         street: {
             elementType: 'input',
@@ -31,7 +39,11 @@ state={
                 type: 'text',
                 placeholder: 'Street'
             },
-            value: ''
+            value: '',
+            validation:{
+                required: true
+            },
+            valid: false
         },
         zipCode: {
             elementType: 'input',
@@ -39,7 +51,13 @@ state={
                 type: 'number',
                 placeholder: 'Zip Code'
             },
-            value: ''
+            value: '',
+            validation:{
+                required: true,
+                minLength: 3,
+                maxLength: 5
+            },
+            valid: false
         },
         country: {
             elementType: 'input',
@@ -47,7 +65,11 @@ state={
                 type: 'text',
                 placeholder: 'Country'
             },
-            value: ''
+            value: '',
+            validation:{
+                required: true
+            },
+            valid: false
         },
         deliveryMethod: {
             elementType: 'select',
@@ -59,6 +81,24 @@ state={
         }
     },
     loading: false
+}
+
+
+checkValidity(value, rules){
+let isValid = true;
+
+if(rules.required){
+    isValid = value.trim() !== '' && isValid;
+}
+
+if(rules.minLength){
+    isValid = value.length >= rules.minLength && isValid;
+}
+
+if(rules.maxLength){
+    isValid = value.length <= rules.maxLength && isValid;
+}
+return isValid;
 }
 
 orderHandler=(event)=>{
@@ -96,8 +136,9 @@ changeHandler= (event, identifier)=>{
     const updatedOrderForm = {...this.state.orderForm};
     const updatedFormElement = {...updatedOrderForm[identifier]};
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
     updatedOrderForm[identifier] = updatedFormElement;
-
+    // console.log(updatedFormElement)
     this.setState({orderForm: updatedOrderForm})
 }
 
